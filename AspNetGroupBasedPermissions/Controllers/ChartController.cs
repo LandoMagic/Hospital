0b51871dec8c4795f25815dea04aedc8dbc399d5
Don <Don@D-PC>
2016-12-18 13:19:24 -0500

    up

---
+++ AspNetGroupBasedPermissions/Controllers/ChartController.cs
@@ -0,0 +1,33 @@
+﻿using HospitalModel;
+using HospitalRepository.DBContext;
+using HospitalWeb.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalWeb.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+        public ActionResult column()
+        {
+            return View();
+
+        }
+
+        public JsonResult GetSCoutnBirth()
+        {
+            var chartViewModel = new ChartsViewModel();
+
+            chartViewModel.countBirth = _db.ChildBirths.Where(a => a.DateOfBirth.Day == DateTime.Now.Day&& a.DateOfBirth.Month == DateTime.Now.Month).Count();
+            chartViewModel.countDeath = _db.Deaths.Count();
+            chartViewModel.countBodyOut = _db.BodyOuts.Count();
+
+            return Json(chartViewModel, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AspNetGroupBasedPermissions/ViewModels/ChartsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using HospitalModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalWeb.ViewModels
+{
+    public class ChartsViewModel
+    {
+       public int countDeath { get; set; }
+        public int countBirth { get; set; }
+        public int countBodyOut { get; set; }
+
+   
+    }
+}
